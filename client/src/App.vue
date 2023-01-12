89279baa4b2c49d9747dc644c35c13652baed07b
eliasyoung <warlockD@126.com>
2023-01-12 18:50:22 +0800

    aside-menu component & some style

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <h1>Hello!</h1>
-  <RouterView />
+  <div class="aside-menu">
+    <AsideRouter v-if="routes.getRoutesList" :routes="routes.getRoutesList" />
+  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoutes } from "@/store/routes";
+import AsideRouter from "./components/AsideRouter.vue";
 
-<style scoped></style>
+const routes = useRoutes();
+</script>
+
+<style scoped>
+.aside-menu {
+  float: left;
+  width: 200px;
+  height: 100%;
+  background-color: rgb(242, 242, 242);
+  padding: 20px;
+}
+
+main {
+  display: inline-block;
+  background-color: white;
+  padding: 20px;
+}
+</style>
---
+++ client/src/components/AsideRouter.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul v-for="route in routes" :key="route.id">
+    <li>
+      <RouterLink :to="route.link ? route.link : route.path">{{
+        route.title
+      }}</RouterLink>
+    </li>
+    <AsideRouter
+      v-if="route.children && route.children.length > 0"
+      :routes="route.children"
+    />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { IRoute } from "@/models";
+
+defineProps<{ routes: IRoute[] }>();
+</script>
+
+<style scoped>
+ul {
+  padding-left: 20px;
+  margin: 10px 0;
+}
+
+ul > li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(69, 69, 69)
+}
+</style>
